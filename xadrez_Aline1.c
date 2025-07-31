4217689003293690c67ed385146c2f211ec0c503
alinepro <prof.alineleobio@gmail.com>
2025-07-31 20:08:14 -0300

    Update xadrez_Aline1.c

--- xadrez_Aline1.c
+++ xadrez_Aline1.c
@@ -4,7 +4,7 @@ int main() {
     int numCasa = 1;
     int i = 1;
     int r = 1;
-    
+    int mov = 1;  //movimento do cavalo
 
     // Mover a Torre usando for
     for (numCasa = 1; numCasa <= 5; numCasa++) {
@@ -22,15 +22,23 @@ int main() {
         printf("Movendo Rainha %d casas para a esquerda.\n",r);
             r++; 
     }
+
+    // Nível Aventureiro - Movimentação do Cavalo
+    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
+    // Um loop pode representar a movimentação horizontal e outro vertical.
+
+    while (mov--){
+        for(int j = 0; j < 2; j++){
+            printf("Cima\n");
+        }
+        printf("Esquerda\n");
+    }
             printf("Saindo do jogo...\n");
 
     return 0;
 }      
    
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
+    
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.

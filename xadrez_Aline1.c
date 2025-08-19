76ebfd356278ea7c066d3d3f73b264c1632576c8
alinepro <prof.alineleobio@gmail.com>
2025-08-18 22:47:16 -0300

    Update xadrez_Aline1.c

--- xadrez_Aline1.c
+++ xadrez_Aline1.c
@@ -1,38 +1,64 @@
 #include <stdio.h>
 
+//Recursividade para movmentar a torre
+void moverTorre(int numCasa){
+    if (numCasa>0){
+    printf("%d\n", numCasa);
+    moverTorre(numCasa-1);
+    }
+}
+void moverBispo(int i){
+    if(i > 0){
+        printf("Movendo Bispo %d casas para cima.\n", i);
+        moverBispo(i - 1);
+    }
+}
+void moverRainha(int j){
+    if(j > 0){
+        printf("Movendo Rainha %d casas para a esquerda.\n", j);
+        moverRainha(j - 1);
+    }
+}
 int main() {
-    int numCasa = 1;
-    int i = 1;
-    int j = 1;
+    moverTorre(5);
+    moverBispo(5);
+    moverRainha(8)
     int mov = 1;  //movimento do cavalo
 
-        // Mover a Torre usando for
-    for (numCasa = 1; numCasa <= 5; numCasa++) {
-         printf("Movendo Torre %d casas para direita.\n", numCasa);
-
-    }
+// Mover a Torre usando for (em comentário, devido ao uso de recursividade)
+    //for (numCasa = 1; numCasa <= 5; numCasa++) {
+         //printf("Movendo Torre %d casas para direita.\n", numCasa);
+    //}
             
-    // Bispo: usando do-while
-    do {    
-        printf("Movendo Bispo %d casas na diagonal cima-direita.\n", i);
-        i++;
-        }  while (i <= 5);         
+    // Bispo: usando loop aninhado, mais externo na vertical, mais interno na horizontal
+    int n = 1;
+        do{
+            int m = 1;
+            do{
+                printf("Movendo Bispo %d casas para esquerda.\n", m);
+                m++;
+            }  while (m <= 5);    
+        printf("Movendo Bispo %d casas para cima.\n", n);
+        n++;
+        } while (n <= 5);
     
     // Rainha: usando while
-    while(j <= 8){
-        printf("Movendo Rainha %d casas para a esquerda.\n",j);
-            j++; 
+   // while(j <= 8){
+     //   printf("Movendo Rainha %d casas para a esquerda.\n",j);
+       //     j++; 
     }
 
        // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    // Um loop pode representar a movimentação horizontal e outro vertical. Uso de loop com continue e break para o cavalo
 
     while (mov--){
         for(int l = 1; l <= 2; l++){
+        if(l == 1) continue;
+            if(l ==2) break;
             printf("Movendo Cavalo %d casa(s) para Cima.\n",l);
         }
-        printf("Movendo Cavalo para Esquerda\n");
+        printf("Movendo Cavalo para direita\n");
     }
             printf("Saindo do jogo...\n");
 
